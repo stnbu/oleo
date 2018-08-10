76c8c5285409117b237b9e79ea0bd77b1411cb09
Mike Burr <mburr@unintuitive.org>
2018-08-10 16:56:39 -0700

    /favicon.ico redirect

--- oleo/urls.py
+++ oleo/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url='/static/main/favicon.ico')),
     path('', RedirectView.as_view(url='/pages/home/')),
     path('pages/', include('main.urls')),
     path('admin/', admin.site.urls),
