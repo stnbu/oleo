c45ca2582cedba09a0afc49044b5828541b7f1e3
Mike Burr <mburr@unintuitive.org>
2018-08-14 15:50:59 -0700

    menu color swap, no /admin/ in production

--- oleo/urls.py
+++ oleo/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.conf import settings
 
 urlpatterns = [
     path('favicon.ico', RedirectView.as_view(url='/static/main/favicon.ico')),
     path('', RedirectView.as_view(url='/pages/home/')),
     path('pages/', include('main.urls')),
-    path('admin/', admin.site.urls),
 ]
+
+# completely disable admin UI in production
+if settings.DEBUG:
+    urlpatterns.append(path('admin/', admin.site.urls))
