95e684e2f40ac9e0f4643d2ad9cbe785c5df77f2
Mike Burr <mburr@unintuitive.org>
2018-08-16 16:08:53 -0700

    allow "hidden" tabs. very imperfect "beta-resume" hosting

--- main/models.py
+++ main/models.py
@@ -5,6 +5,7 @@ class Page(models.Model):
     name = models.CharField(max_length=30)
     label = models.CharField(max_length=60)
     headline = models.CharField(max_length=200)
+    tab_visible = models.BooleanField('Should tab be visible?', default=True)
 
     def __str__(self):
         return self.name
