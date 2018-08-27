457e0cfc42c801532df82148082318ceacc43e50
Mike Burr <mburr@unintuitive.org>
2018-08-26 17:06:05 -0700

    resume -> resume.html

--- oleo/urls.py
+++ oleo/urls.py
@@ -5,6 +5,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('favicon.ico', RedirectView.as_view(url='/static/main/favicon.ico')),
+    path('resume/', RedirectView.as_view(url='/static/main/bin/resume-mike-burr.html')),
     path('', RedirectView.as_view(url='/pages/home/')),
     path('pages/', include('main.urls')),
 ]
